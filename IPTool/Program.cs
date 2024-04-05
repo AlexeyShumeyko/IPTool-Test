df8352d0e162176a3d2c0131745bb02a3de3ab45
AlexeyShumeyko <schumeiko.alexei@yandex.by>
2024-04-05 13:49:15 +0300

    Initialization of receiving entered parameters; Checking the received parameters

--- IPTool/Program.cs
+++ IPTool/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using IPTool;
+
+var logFilePath = Startup.GetFilePathArg(args, Startup.FILE_LOG);
+var outputFilePath = Startup.GetFilePathArg(args, Startup.FILE_OUTPUT);
+var addressStart = Startup.GetFilePathArg(args, Startup.ADDRESS_START);
+var addressMask = Startup.GetFilePathArg(args, Startup.ADDRESS_MASK);
+
+var timeStart = Startup.GetTimeArg(args, Startup.TIME_START);
+var timeEnd = Startup.GetTimeArg(args, Startup.TIME_END);
+
+if (string.IsNullOrEmpty(logFilePath) || string.IsNullOrEmpty(outputFilePath) 
+    || timeStart == DateTime.MinValue || timeEnd == DateTime.MinValue)
+{
+    Console.WriteLine("Missing required parameters");
+    return;
+}
