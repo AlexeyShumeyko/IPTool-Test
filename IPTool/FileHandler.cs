e68858447fe44d635e5a9878a198c48746b1fbbb
AlexeyShumeyko <schumeiko.alexei@yandex.by>
2024-04-05 17:21:54 +0300

    Adding a class to manage working with files; Implementing a method to read from a file

---
+++ IPTool/FileHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPTool
+{
+    internal class FileHandler
+    {
+        internal static List<string> ReadLogFromFile (string path)
+        {
+            try
+            {
+                return File.ReadAllLines(path).ToList();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error reading log file: " + ex.Message);
+                return new List<string>();
+            }
+        }
+    }
+}
