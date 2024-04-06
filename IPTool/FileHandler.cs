26d7837f06ed49b9d49f6212ed0b6be0132107ab
AlexeyShumeyko <schumeiko.alexei@yandex.by>
2024-04-06 12:28:56 +0300

    Adding output of a filtered list of IP addresses to a file

--- IPTool/FileHandler.cs
+++ IPTool/FileHandler.cs
@@ -20,5 +20,26 @@ namespace IPTool
                 return new List<string>();
             }
         }
+
+        internal static void SaveResults (Dictionary<string, int> ipAddresses, string outputFilePath)
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(outputFilePath))
+                {
+                    foreach (var result in ipAddresses)
+                    {
+                        sw.WriteLine($"{result.Key}:{result.Value}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error writing to output file: " + ex.Message);
+                return;
+            }
+
+            Console.WriteLine("Success!");
+        }
     }
 }
